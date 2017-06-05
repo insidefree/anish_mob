273ea9c43c468e8feadc5e7753bd70f3be0c9d6a
Denis Sorokin <denys-s@yit.co.il>
2017-06-05 09:29:54 +0300

    Added firebase config

--- main.js
+++ main.js
@@ -5,6 +5,20 @@ import store from './src/store'
 import HomeScreen from './screens/HomeScreen'
 
 class App extends React.Component {
+
+  componentWillMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDHKja4zGoInK7D5PsL4MCHAYC2lmvGGPA",
+      authDomain: "anish-6cd8e.firebaseapp.com",
+      databaseURL: "https://anish-6cd8e.firebaseio.com",
+      projectId: "anish-6cd8e",
+      storageBucket: "anish-6cd8e.appspot.com",
+      messagingSenderId: "613075498242"
+    }
+    firebase.initializeApp(config)
+  }
+
   render() {
     return (
       <Provider store={store}>
