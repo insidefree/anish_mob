bf44ce3af98a2f37d975bb0ef17adb43c744fb12
Denis Sorokin <denys-s@yit.co.il>
2017-06-12 11:50:15 +0300

    Work on AnimalCard

--- src/components/AnimalCard.js
+++ src/components/AnimalCard.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, StyleSheet, Text, ListView, Image, ScrollView } from 'react-native'
 import { connect } from 'react-redux'
 
-
+const uri = 'https://static.pexels.com/photos/58997/pexels-photo-58997.jpeg'
+const keyTEMP = 1
 class AnimalCard extends Component {
 
     render() {
-        console.log("AnimalCard")
-        console.log(" ")
-        console.log(" ")
-        console.log(this.props)
         const { animal } = this.props
         return (
-            <View>
-                <Text>{animal.sub_id}</Text>
+            <View style={{ flex: 1 }}>
+                <Text>Animal ID: {animal.sub_id}</Text>
+                <ScrollView>
+                    {animal.images.map(link => <Image key={keyTEMP++} style={styles.animalImg} source={{ uri: link }} />)}
+                </ScrollView>
+                <Text>Description: {animal.description}</Text>
+                <Text>Location: City - {animal.location.city}, Street - {animal.location.street}</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    animalImg: {
+        flex: 1,
+        height: 200,
+        width: null
+    }
+});
 
 
 export default connect(null)(AnimalCard)
