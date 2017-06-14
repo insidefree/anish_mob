425f3a5438fb62cfff3009b494b23e69f966268a
Denis Sorokin <denys-s@yit.co.il>
2017-06-14 15:33:03 +0300

    Added const to Login reducer

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,12 +1,14 @@
+import { LOGIN_USER } from '../constants/'
+
 const INIT_STATE = {
-	email: null
+  email: null
 };
 
 export default (state = INIT_STATE, action) => {
   switch (action.type) {
-    case "LOGIN_USER":
+    case LOGIN_USER:
       return { email: action.payload };
     default:
-			return state;
+      return state;
   }
 }
