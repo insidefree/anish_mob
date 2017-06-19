929ccdc9611643b350168eb1682df334000e5c98
Denis Sorokin <denys-s@yit.co.il>
2017-06-19 13:41:03 +0300

    Added header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {
+  Text,
+  View
+} from 'react-native';
 
+const styles = {
+  textStyle: {
+    fontSize: 20,
+    color: '#383838'
+  },
+  viewStyle: {
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.8,
+    elevation: 8,
+    position: 'relative'
+  }
+};
 
-export class Header extends Component {
-    render() {
-        return (
-            <View>
-                <Text>
-                    HEADER
-                </Text>
-            </View>
-        )
-    }
-}
+const Header = ({ title }) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{ title }</Text>
+    </View>
+  );
+};
 
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+};
+
+export { Header };
