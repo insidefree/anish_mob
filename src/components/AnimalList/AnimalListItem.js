627bfc35edfa3c11acd1ca9d2325b85f5b0e5c62
Denis Sorokin <denys-s@yit.co.il>
2017-06-20 13:40:53 +0300

    Added animal card styles

--- src/components/AnimalList/AnimalListItem.js
+++ src/components/AnimalList/AnimalListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 
 
 const uri = 'https://static.pexels.com/photos/58997/pexels-photo-58997.jpeg';
@@ -8,20 +8,43 @@ export class AnimalListItem extends Component {
         const { animal } = this.props
         console.log(animal)
         return (
-            <View style={{ marginVertical: 8, padding: 20 }}>
+            <View style={{
+                marginVertical: 8,
+                margin: 20,
+                paddingTop: 15,
+                paddingBottom: 30,
+                borderBottomColor: '#9f9f9f',
+                borderBottomWidth: 1,
+                borderStyle: 'dotted'
+            }}>
                 <Image style={styles.imageStyle} source={{ uri }} />
                 <View style={styles.cardStyle}>
-                    <Image source={require('../../assets/Menu-button.png')} style={{
-                        width: 50,
-                        height: 40
+                    <Image source={require('../../assets/name.png')} style={{
+                        height: 30.7,
+                        width: 27,
                     }} />
-                    <Text>
-                        Description: {animal.description}
-                    </Text>
-                    <Text>
-                        Location: City - {animal.location.city}, Street - {animal.location.street}
-                    </Text>
+                    <Text>{animal.name}</Text>
+                    <Image source={require('../../assets/age.png')} style={{
+                        height: 30.7,
+                        width: 27,
+                    }} />
+                    <Text>{animal.age}</Text>
+                    <Image source={require('../../assets/location.png')} style={{
+                        height: 30.7,
+                        width: 21,
+                    }} />
+                    <Text>{animal.location.city}</Text>
                 </View>
+                <Text style={styles.description}>
+                    Description: {animal.description}
+                </Text>
+                <TouchableOpacity
+                    onPress={() => console.log('btn pressed')}
+                >
+                    <View style={styles.btn}>
+                        <Text style={styles.btnText}>take me...</Text>
+                    </View>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -36,9 +59,11 @@ const styles = {
         borderWidth: 1,
     },
     cardStyle: {
-        padding: 8,
-        borderColor: '#e0e0e0',
-        borderWidth: 1,
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 8
     },
     scrollView: {
         height: 300,
@@ -50,6 +75,26 @@ const styles = {
         height: 200,
         flex: 1,
         width: null,
+        borderRadius: 15,
+        marginBottom: 10
     },
+    btn: {
+        height: 30,
+        width: null,
+        marginTop: 20,
+        borderRadius: 50,
+        backgroundColor: '#EE5350',
+    },
+    description: {
+        marginTop: 10
+    },
+    btnText: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        fontSize: 20,
+        color: '#fff'
+    }
 };
 
