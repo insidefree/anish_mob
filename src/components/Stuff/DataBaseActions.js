97912c188d8afb3a2190af5c2902ca9b61ed63fa
Denis Sorokin <denys.sorokin@gmail.com>
2017-08-26 10:45:55 +0300

    added nativebase

--- src/components/Stuff/DataBaseActions.js
+++ src/components/Stuff/DataBaseActions.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 // import { View, Text, TextInput, ToastAndroid, Platform } from 'react-native'
-import { connectStyle, Button, Text, Icon, Container, Content } from 'native-base';
+import { connectStyle, Button, Text, Icon, Container, Content, StyleProvider } from 'native-base';
+import getTheme from '../../../native-base-theme/components'
+import platform from '../../../native-base-theme/variables/platform'
+
 // import { Button } from 'react-native-elements'
 
 // config
@@ -37,51 +40,50 @@ export default class DataBaseActions extends Component {
 
     render() {
         return (
-            <Container style={{ flex: 1,
-                backgroundColor: '#fff',
-                alignItems: 'center',
-                justifyContent: 'center'}}>
-                <Content>
-                    <Button
-                        block
-                        info
-                        onPress={() => this.onSubmit()}
-                    >
-                        <Text>Upload fake data to the server</Text>
-                    </Button>
-                    <Button
-                        block
-                        info
-                        onPress={() => this.onClear()}
-                    >
-                        <Text>Clear animals data</Text>
-                    </Button>
-                    <Button
-                        block
-                        info
-                        onPress={() => this.showAnimalsFromDB()}
-                    >
-                        <Text>Show animals from DB - console</Text>
-                    </Button>
-                    <Button
-                        block
-                        info
-                        onPress={() => this.props.navigation.navigate('AddFormScreen')}
-                    >
-                        <Text>Move to AddFormScreen - test add animal form</Text>
-                    </Button>
-                </Content>
-            </Container>
+            <StyleProvider style={getTheme(platform)}>
+                <Container>
+                    <Content padder>
+                        <Button
+                            block
+                            info
+                            onPress={() => this.onSubmit()}
+                        >
+                            <Text>Upload fake data to the server</Text>
+                        </Button>
+                        <Button
+                            block
+                            info
+                            onPress={() => this.onClear()}
+                        >
+                            <Text>Clear animals data</Text>
+                        </Button>
+                        <Button
+                            block
+                            info
+                            onPress={() => this.showAnimalsFromDB()}
+                        >
+                            <Text>Show animals from DB - console</Text>
+                        </Button>
+                        <Button
+                            block
+                            info
+                            onPress={() => this.props.navigation.navigate('AddFormScreen')}
+                        >
+                            <Text>Move to AddFormScreen - test add animal form</Text>
+                        </Button>
+                    </Content>
+                </Container>
+            </StyleProvider>
         )
     }
 }
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: '#fff',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
