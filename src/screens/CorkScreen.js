23ca1833f6bd3a6549a4ee9d44649260ceef1f6d
Denis Sorokin <denys-s@yit.co.il>
2017-10-11 10:27:50 +0200

    BackHandler android test

--- src/screens/CorkScreen.js
+++ src/screens/CorkScreen.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, BackHandler } from 'react-native'
 
 
 export default class CorkScreen extends Component {
+    componentDidMount() {
+        // BackHandler.addEventListener('hardwareBackPress', function () {
+        //     const { dispatch, navigation, nav } = this.props;
+        //     if (nav.routes.length === 1 && (nav.routes[0].routeName === 'Login' || nav.routes[0].routeName === 'Start')) {
+        //         return false;
+        //     }
+        //     // if (shouldCloseApp(nav)) return false
+        //     dispatch({ type: 'Navigation/BACK' });
+        //     return true;
+        // }.bind(this));  
+
+        BackHandler.addEventListener('hardwareBackPress', function() {
+            // this.onMainScreen and this.goBack are just examples, you need to use your own implementation here
+            // Typically you would use the navigator here to go to the last state.
+            console.log('BACK')
+            
+           });
+    }
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress')
+    }
     render() {
         return (
             <View style={styles.container}>
